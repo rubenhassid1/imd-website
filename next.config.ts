43475036444452c0ec7b49cbdd6e26b3080eb8bc
rubenhassid1 <rubenhassid1@users.noreply.github.com>
2026-03-22 16:08:59 +0200

    Add basePath for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/imd-website",
+  assetPrefix: "/imd-website/",
 };
 
 export default nextConfig;
